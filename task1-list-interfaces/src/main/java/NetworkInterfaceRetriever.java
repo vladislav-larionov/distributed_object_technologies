8561c8bef03178215c640d4039b12f89587e5d2e
Vladislav Larionov <vladislav.larionov@fruct.org>
2021-03-01 21:40:58 +0300

    Fix the bug when the application is crashed when a mac address is null

--- task1-list-interfaces/src/main/java/NetworkInterfaceRetriever.java
+++ task1-list-interfaces/src/main/java/NetworkInterfaceRetriever.java
@@ -17,9 +17,17 @@ public class NetworkInterfaceRetriever {
         return ip.getLocalHost().getHostAddress();
     }
 
-    public String getMacAddress() throws SocketException {
+    public String getMacAddress() throws SocketException, UnknownHostException {
         NetworkInterface network = NetworkInterface.getByInetAddress(ip);
-        byte[] mac = network.getHardwareAddress();
+        if (network == null)
+            return "None";
+        return byteMacToString(network.getHardwareAddress());
+
+    }
+
+    private String byteMacToString(byte[] mac) {
+        if (mac == null)
+            return "None";
         StringBuilder builtMac = new StringBuilder();
         for (int i = 0; i < mac.length; i++) {
             builtMac.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
@@ -31,14 +39,16 @@ public class NetworkInterfaceRetriever {
         return Collections.list(NetworkInterface.getNetworkInterfaces());
     }
 
-    public void printNetworkInterfaces() throws SocketException {
-        for (NetworkInterface netInterface : getNetworkInterfaces())
+    public void printNetworkInterfaces() throws SocketException, UnknownHostException {
+        for (NetworkInterface netInterface : getNetworkInterfaces()) {
             printInterface(netInterface);
+        }
     }
 
-    private void printInterface (NetworkInterface netInterface) {
+    private void printInterface (NetworkInterface netInterface) throws SocketException, UnknownHostException {
         System.out.printf("Display name: %s\n", netInterface.getDisplayName());
         System.out.printf("Name: %s\n", netInterface.getName());
+        System.out.printf("MAC: %s\n", byteMacToString(netInterface.getHardwareAddress()));
         Enumeration<InetAddress> inetAddresses = netInterface.getInetAddresses();
         for (InetAddress inetAddress : Collections.list(inetAddresses)) {
             System.out.printf("InetAddress: %s\n", inetAddress);
